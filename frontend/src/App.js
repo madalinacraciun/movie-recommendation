7425ca983476ed9ac363c9ca8b2e60be7720843d
Dinu Cosmin Stefan <cosminstefandinu@gmail.com>
2020-12-29 17:01:04 +0200

    commit after pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,44 @@
-import './App.css';
-import SearchBar from './SearchBar'
-import Movie from './Movie';
-
-
+import { React, useState } from "react";
+import "./App.css";
+import SearchBar from "./SearchBar";
+import Movie from "./Movie";
 
 function App() {
-  const data = ["Thor","Thor: The Dark World","Thor 2","Thor 3"]
+  const data = ["Thor", "Thor: The Dark World", "Thor 2", "Thor 3"];
+  const [searchText, setSearchText] = useState("");
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://127.0.0.1:5000/search-movie", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "no-cors", // no-cors, *cors, same-origin
+      // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ keyword: searchText }), // body data type must match "Content-Type" header
+    });
+    // const data = await response.json();
+    // console.log(data);
+  };
   return (
     <div className="container">
       <h3>Movie recomander app</h3>
-      
-      <SearchBar/>
-      <Movie/>
-      
+
+      <SearchBar
+        searchText={searchText}
+        setSearchText={setSearchText}
+        handleSearch={handleSearch}
+      />
+      <Movie />
+
       <div className="search-results">
-        {data.map(item=>(
-          <span key="item" className="badge bg-primary m-2 p-2">{item}</span>
-        )
-        )}
-       
+        {data.map((item) => (
+          <span key={item} className="badge bg-primary m-2 p-2">
+            {item}
+          </span>
+        ))}
       </div>
-      
     </div>
   );
 }
--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = ()=>{
-    const handleSearch = async(e)=>{
-        e.preventDefault();
-
-    }
-    return(
+const SearchBar = ({ searchText, setSearchText, handleSearch }) => {
+  return (
     <form className="form-inline mr-auto d-flex" onSubmit={handleSearch}>
-        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-primary btn-rounded btn-sm my-0" type="submit">Search</button>
+      <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="form-control mr-sm-2"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button
+        className="btn btn-outline-primary btn-rounded btn-sm my-0"
+        type="submit"
+      >
+        Search
+      </button>
     </form>
-    )
-}
+  );
+};
 export default SearchBar;
